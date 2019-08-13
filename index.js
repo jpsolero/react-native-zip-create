d1806de2b0e5dbe4e81a66cbb8e4ffbe4fe6bd76
Javier Perez-Solero <jpsolero@mrmilu.com>
2019-08-13 19:42:04 +0200

    fixed event

--- index.js
+++ index.js
@@ -13,9 +13,8 @@ export const zip = (source, target) => {
 
 const zipCreateEmitter = new NativeEventEmitter(ZipCreate);
 
-
 export const subscribe = callback => {
-  return calendarManagerEmitter.addListener(
-    'EventReminder', callback
+  return zipCreateEmitter.addListener(
+    'zipCreateProgress', callback
   );
 }
