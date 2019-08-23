77abb898d06b58ef8029fcf191f6da32724ddbad
Javier Perez-Solero <jpsolero@mrmilu.com>
2019-08-23 21:45:03 +0200

    fixed import

--- android/src/main/java/com/reactlibrary/ZipCreateModule.java
+++ android/src/main/java/com/reactlibrary/ZipCreateModule.java
@@ -13,6 +13,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import java.io.File;
